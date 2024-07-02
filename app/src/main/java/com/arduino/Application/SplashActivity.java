eaf269a43dbf2fc9abc041c7b770e9eb4de8909b
CHOYN <javacyn@naver.com>
2024-07-03 02:02:53 +0900

    splash update

---
+++ app/src/main/java/com/arduino/Application/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.arduino.Application;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        //splash 1초 동안 뜨게 함.
+        final Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 1000);//1000은 1초다.
+       // postDelayed 매소드를 통해 1초 뒤에 splashHandler 작동하도록 설정
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    //splashHandler 클래스 생성
+    private class splashHandler implements Runnable {
+        @Override
+        public void run() {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+}
