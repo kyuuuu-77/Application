a3a924e9c25fa90f0cada29765817c229af1a031
CHOYN <javacyn@naver.com>
2024-07-03 13:05:57 +0900

    splash update1

--- app/src/main/java/com/arduino/Application/SplashActivity.java
+++ app/src/main/java/com/arduino/Application/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
 
         //splash 1초 동안 뜨게 함.
         final Handler hd = new Handler();
-        hd.postDelayed(new splashHandler(), 2000);//1000은 1초다. 2초로 설정
+        hd.postDelayed(new splashHandler(), 2000);//1000은 1초다. 2초로 설정.
        // postDelayed 매소드를 통해 2초 뒤에 splashHandler 작동하도록 설정
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
